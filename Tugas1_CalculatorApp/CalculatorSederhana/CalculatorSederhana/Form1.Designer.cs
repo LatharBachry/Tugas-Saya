8436742b233c2798571c1f69758914a0ac697c96
LatharBachry <41373021+LatharBachry@users.noreply.github.com>
2018-09-27 23:30:19 +0700

    LatharBachry
    Tugas saya

---
+++ Tugas1_CalculatorApp/CalculatorSederhana/CalculatorSederhana/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace CalculatorSederhana
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Nilai_1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Keluaran = new System.Windows.Forms.TextBox();
+            this.Nilai_2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Nilai_1
+            // 
+            this.Nilai_1.Location = new System.Drawing.Point(24, 49);
+            this.Nilai_1.Name = "Nilai_1";
+            this.Nilai_1.Size = new System.Drawing.Size(73, 22);
+            this.Nilai_1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Location = new System.Drawing.Point(38, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 19);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Input";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "+",
+            "-",
+            "x",
+            ":"});
+            this.comboBox1.Location = new System.Drawing.Point(103, 49);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(31, 24);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(95, 77);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(50, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "GO";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Keluaran
+            // 
+            this.Keluaran.Location = new System.Drawing.Point(87, 106);
+            this.Keluaran.Name = "Keluaran";
+            this.Keluaran.Size = new System.Drawing.Size(70, 22);
+            this.Keluaran.TabIndex = 4;
+            // 
+            // Nilai_2
+            // 
+            this.Nilai_2.Location = new System.Drawing.Point(142, 49);
+            this.Nilai_2.Name = "Nilai_2";
+            this.Nilai_2.Size = new System.Drawing.Size(73, 22);
+            this.Nilai_2.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Location = new System.Drawing.Point(160, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 19);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Input";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei Light", 7.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label3.Location = new System.Drawing.Point(198, 175);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Latharium";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label4.Location = new System.Drawing.Point(28, 109);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 19);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Output";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(77, 134);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(93, 23);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "CLEAR";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(271, 199);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Nilai_2);
+            this.Controls.Add(this.Keluaran);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Nilai_1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Text = "Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Nilai_1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox Keluaran;
+        private System.Windows.Forms.TextBox Nilai_2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Tugas1_CalculatorApp/CalculatorSederhana/CalculatorSederhana/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculatorSederhana
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = int.Parse(Nilai_1.Text);//*Proses Casting dari String ke Integer
+            int b = int.Parse(Nilai_2.Text);
+            Double p_Keluaran; // Penyimpanan Sementara untuk Var a & b
+
+            switch(comboBox1.SelectedIndex)
+            {
+                case 0:
+                    p_Keluaran = a + b;
+                    Keluaran.Text = Convert.ToString(p_Keluaran);
+                    break;
+                case 1 :
+                    p_Keluaran = a - b;
+                    Keluaran.Text = Convert.ToString(p_Keluaran);
+                    break;
+                case 2 :
+                    p_Keluaran = a * b;
+                    Keluaran.Text = Convert.ToString(p_Keluaran);
+                    break;
+                case 3 :
+                    p_Keluaran = a / b;
+                    Keluaran.Text = Convert.ToString(p_Keluaran);
+                    break;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e) //Fungsi untuk Clear Data
+        {
+            Nilai_1.Text = "";
+            Nilai_2.Text = "";
+            Keluaran.Text = "";
+        }
+    }
+}
